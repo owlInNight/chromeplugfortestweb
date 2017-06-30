88bac378391998f52d98f4df930a7698dc554cba
noSleepOwl <844910815@qq.com>
2017-06-30 10:00:52 +0800

    玩玩记得在 使用 直接执行的方法后面添加一个 分号 要不会有bug 的  太特么的蛋疼了 , 查了半个多小时

--- watchsource/WebContent/jsfile/myjs/my.js
+++ watchsource/WebContent/jsfile/myjs/my.js
@@ -1,25 +1,81 @@
 $(function() {
-    $("body").append("hello world");
-    //test extend
+	var option = {
+		width : 10,
+		height : 20
+	};
+	var Tetris = function(option) {
+		this.option = option;
+		this.isStart = false;
+		// 窗口元素
+		var body = this.window = $('[data-toggle="Tetris"]');
+		// 初始化方法
+		this.init();
+	}
+	var ext = Tetris.prototype;
+	ext.init = function() {
+		this.initWindow();
+	};
+	// 主窗口
+	ext.initWindow = function() {
+		var that = this, option = that.option;
+		/**
+		 * 创建 窗体的格子
+		 */
+		var createMainWindow = function(width, height) {
+			var tr = "";
+			for (var i = 0; i < height; i++) {
+				var td = "";
+				for (var j = 0; j < width; j++) {
+					td += `<td td-index="${j}" ></td>`;
+				}
+				tr += `<tr tr-index="${i}">${td}</tr>`;
+			}
+			return `<table >${tr} </table>`;
+		}
+		that.window.append(createMainWindow(option.width, option.height));
+	};
+	/**
+	 * 开始游戏
+	 */
+	ext.startGame = function() {
+		this.isStart = true;
+		if (isStart) {
+			this.keyPress();
+			this.createBox();
+		}
+	};
+	ext.keyPress = function() {
+	};
+	ext.createBox = function() {
 
-    var res = $.fn.extend({}, {
-        name: 'aaa',
-        age: 'bbb',
-        work: 'ccc'
-    });
+		var that = this, height = that.option.height;
+
+		var newBox = function(x, y) {
+			var tar = $(`[tr-index="y"] [td-index="x"]`);
+			tar.addClass("activeBox");
+		}
+
+		if (height == 0) {
+			Window.clearInterval(code);
+			that.createBox();
+		} else {
+			var code = setInterval(function() {
+				newBox(height, 3)
+				height--;
+			}, 1000)
+		}
+	};
+	/**
+	 * 初始化
+	 */
+	(function() {
+		var tetris = new Tetris(option);
+		/**
+		 * 开始游戏 按钮
+		 */
+		$("#startGame").click(function() {
+			tetris.startGame();
+		});
+
+	})()
 });
-var tes = {
-    name: 'aaa',
-    age: 'bbb',
-    work: 'ccc'
-}
-
-var res_1 = $.fn.extend(tes)
-    console.log(res_1);
-    console.log(res_1.name);
-
- var res_2 =  $.type(res_1);
-
-
-
- console.log(res_2)
